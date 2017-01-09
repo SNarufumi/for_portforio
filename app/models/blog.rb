bd4235328911b2fafbb6798aca200f9cf8bc4a0e
Narufumis <shirota.narufumi.p0@tufs.ac.jp>
2017-01-09 21:54:38 +0900

    スクレイピング断念

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
   validates :title, presence: {message: "タイトルを入力してください"}
   validates :content, presence: {message: "内容を入力してください"}
   acts_as_taggable_on :category       
+  belogs_to:user
 end
